75ad4a5a3a70859e102a7e056a336f2f5836e865
Badjatya <architj240@gmail.com>
2023-05-25 23:28:14 +0530

    [Feat]: JS Assignment Q1

---
+++ Javascript/01 Question/variable.js
@@ -0,0 +1,18 @@
+let name = "Archit";
+let isStudent = true;
+let isInHighSchool = false;
+let age = null;
+let food = undefined;
+
+console.log("Name: " + name + " and its data type is " + typeof name);
+console.log(
+    "Student: " + isStudent + " and its data type is " + typeof isStudent
+);
+console.log(
+    "In High School: " +
+        isInHighSchool +
+        " and its data type is " +
+        typeof isInHighSchool
+);
+console.log("Age: " + age + " and its data type is " + typeof age);
+console.log("Food: " + food + " and its data type is " + typeof food);
