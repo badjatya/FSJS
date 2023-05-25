80e8ec1b498049f963f0291d353cba7c54ff7210
Badjatya <architj240@gmail.com>
2023-05-25 23:31:32 +0530

    [Feat]: JS Assignment Q2

---
+++ Javascript/02 Question/index.js
@@ -0,0 +1,11 @@
+// 02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+const firstName = "Archit";
+const lastName = "Badjatya";
+const maritalStatus = "Single";
+const country = "India";
+const age = 21;
+
+console.log(
+    `I am ${firstName} ${lastName} and I am from ${country}, my age is ${age} and I am ${maritalStatus}`
+);
