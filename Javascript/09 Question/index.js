22630735cc978a8b2af93a81c151344f528f9859
Badjatya <architj240@gmail.com>
2023-05-26 12:05:24 +0530

    [Feat]: JS Assignment Q9

---
+++ Javascript/09 Question/index.js
@@ -0,0 +1,13 @@
+// 09. Boolean value is either true or false.
+// - Write three JavaScript statement example which provide truthy value.
+// - Write three JavaScript statement example which provide falsy value.
+
+// Truthy Values
+const age = 10;
+const name = "Archit";
+const obj = {};
+
+// Falsy values
+const year = 0;
+const surname = "";
+const number = null;
