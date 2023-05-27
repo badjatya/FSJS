bbbef446d5bf7bf094d59d0e1f4adaf3dd96a5af
Badjatya <architj240@gmail.com>
2023-05-27 21:56:34 +0530

    [Feat]: DOM ASSIGNMENT 1

---
+++ DOM/01 DOM Assignment/index.js
@@ -0,0 +1,13 @@
+let logo = document.querySelector(".logo");
+let avatar = document.querySelector(".hero-right-section img");
+let github = document.querySelector("a.github");
+let linkedIn = document.querySelector("a.linkedin");
+let instagram = document.querySelector("a.instagram");
+
+logo.innerText = "Archit";
+
+avatar.src =
+    "https://pbs.twimg.com/profile_images/1597885692730044417/kAqLxUSx_400x400.jpg";
+
+github.href = "https://github.com/badjatya";
+linkedIn.href = "https://www.linkedin.com/in/badjatya/";
